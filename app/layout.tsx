17047d6f5ecd78bde0a2fa6d89e2503d5cf16306
bishnt <bishrant150@gmail.com>
2025-07-22 21:11:42 +0545

    fix: zoom error for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ClientLayout from "@/components/ClientLayout";
 export const metadata = {
   title: "AGRIHA ‑ Find Homes, Far From Home",
   description: "",
+   viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
